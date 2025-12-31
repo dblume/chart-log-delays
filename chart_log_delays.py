08ac55a0c517499f1a383eabe6e6b735d8318a3d
dblume <david.blume@gmail.com>
2025-12-30 22:55:31 -0800

    First cut at calculating time differences

--- chart_log_delays.py
+++ chart_log_delays.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+# git ls-files | entr -c sh -c 'ctags -R *; ./chart_log_delays.py'
 
 import os
 import sys
 from argparse import ArgumentParser
 from typing import Callable
 from pathlib import Path
+from collections import namedtuple
+import datetime
 import traceback
 
 __author__ = "David Blume"
-__copyright__ = "Copyright 2016-2022, David Blume"
+__copyright__ = "Copyright 2025, David Blume"
 __license__ = "MIT"
 __version__ = "1.0"
 __status__ = "Development"
@@ -27,41 +30,33 @@ def filenum_excepthook(exc_type, exc_value, tb):
 
 sys.excepthook = filenum_excepthook
 
-# This class demonstrates decorators.
-#
-# Consider these alternatives for data classes:
-# * dataclasses.dataclass: sensible defaults, compares class type, can be frozen
-#                          See "bored" module for data validation with marshmallow
-# * collections.namedtuple: esp. useful for results of csv and sqlite3
-class Coffee:
+Log = namedtuple('Log', ['first_occur', 'frequency_s', 'filename', 'url'])
 
-    def __init__(self, price: float):
-        super().__init__()  # See https://eugeneyan.com/writing/uncommon-python/
-        self._price = price
+Logs = [#Log('2024-12-22, 20:30',            -1,      'fm_stats.txt',     'https://david.dlma.com/location/fm_stats.txt'),
+        #Log('Sat Dec 20 12:30:00 PST 2025', 60 * 60, 'dreamhost_30.txt', 'https://dblu.me/dreamhost_30.txt'),
+        Log('Sat Dec 20 12:00:00 PST 2025', 60 * 60, 'dreamhost_00.txt', 'https://dblu.me/dreamhost_00.txt'),
+        #Log('Thu Dec 18 11:00:00 PST 2025', 30 * 60, 'dreamhost.txt',    'https://dblu.me/dreamhost.txt'),
+       ]
 
-    @property
-    def price(self) -> float:
-        return self._price
-
-    @price.setter
-    def price(self, new_price: float) -> None:
-        if new_price > 0 and isinstance(new_price, float):
-            self._price = new_price
-        else:
-            raise ValueError("price must be a non-negative float.")
-
+# We'll have to be clever about processing fm_stats.txt because it has various offsets.
+# 9:30, 20:30: https://david.dlma.com/location/fm_stats.txt
 
 def main(debug: bool) -> None:
     script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-    print(f'{sys.argv[0]} is in {script_dir}.')
+    #print(f'{sys.argv[0]} is in {script_dir}.')
 
-
-    if debug and (Path.home() / 'bin').exists():
-        print('Your home directory has a bin/ directory.')
-
-    cuppa = Coffee(5.00)
-    cuppa.price = cuppa.price - 1.00
-    print(f'Coffee on sale for ${cuppa.price:1.2f}.')
+    for log in Logs:
+        expected = datetime.datetime.strptime(log.first_occur, '%a %b %d %H:%M:%S %Z %Y')
+        print(f'{log.filename} updates every {log.frequency_s} seconds, first at {log.first_occur}, {expected=}')
+        with open(log.filename, 'r', encoding='utf-8') as f:
+            i = 0
+            for line in f:
+                i += 1
+                observed = datetime.datetime.strptime(line[:28], '%a %b %d %H:%M:%S %Z %Y')
+                print(f"{observed} - {expected} = {observed - expected}")
+                expected = expected + datetime.timedelta(seconds=log.frequency_s)
+                if i >= 10:
+                    break
 
 
 if __name__ == '__main__':
