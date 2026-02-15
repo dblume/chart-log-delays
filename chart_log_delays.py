08cf9f00408c3441d77f7e09078658d5ae8a5e69
dblume <david.blume@gmail.com>
2026-02-14 18:18:47 -0800

    Automatically download log files.

--- chart_log_delays.py
+++ chart_log_delays.py
@@ -10,6 +10,7 @@ from collections import namedtuple
 import datetime
 import traceback
 import subprocess
+import requests
 
 __author__ = "David Blume"
 __copyright__ = "Copyright 2025, David Blume"
@@ -34,12 +35,12 @@ sys.excepthook = filenum_excepthook
 Log = namedtuple('Log', ['hour_offset', 'frequency_s', 'filename', 'url'])
 
 Logs = [Log(30, -1,      'fm_stats.txt',     'https://david.dlma.com/location/fm_stats.txt'),
-        #Log(30, 60 * 60, 'dreamhost_30.txt', 'https://dblu.me/dreamhost_30.txt'),
+        Log(30, 60 * 60, 'dreamhost_30.txt', 'https://dblu.me/dreamhost_30.txt'),
         Log(00, 60 * 60, 'dreamhost_00.txt', 'https://dblu.me/dreamhost_00.txt'),
-        #Log(00, 30 * 60, 'dreamhost.txt',    'https://dblu.me/dreamhost.txt'),
+        #Log(00, 30 * 60, 'dreamhost.txt',    'https://dblu.me/dreamhost.txt'), # <-- half hour cadence, but 1h timeouts cause ambiguity about which run is which.
        ]
 
-CRON_TIMEOUT = 60 * 60  # 1 hour
+CRON_TIMEOUT_S = 60 * 60  # 1 hour
 
 
 def next_expected(prev_expected: datetime.datetime, frequency_s: int) -> datetime.datetime:
@@ -58,24 +59,34 @@ def main(debug: bool) -> None:
     #print(f'{sys.argv[0]} is in {script_dir}.')
 
     for log in Logs:
-        print(f'{log.filename} updates every {log.frequency_s}')
+        # Download the log file if it doesn't exist or is more than 1 hour old.
+        log_path = Path(script_dir) / log.filename
+        if not log_path.exists() or (datetime.datetime.now() - datetime.datetime.fromtimestamp(log_path.stat().st_mtime)).total_seconds() > 60 * 60:
+            print(f"Downloading {log.url} to {log.filename}...")
+            response = requests.get(log.url)
+            response.raise_for_status()
+            with open(log.filename, 'w', encoding='utf-8') as f:
+                f.write(response.text)
+
         with open(log.filename, 'r', encoding='utf-8') as f:
             i = 0
             scheduled_runs = list()
             if log.frequency_s > 0:
-                max_duration_s = min(log.frequency_s, CRON_TIMEOUT)
+                max_duration_s = min(log.frequency_s, CRON_TIMEOUT_S)
             else:
-                max_duration_s = CRON_TIMEOUT
+                max_duration_s = CRON_TIMEOUT_S
             for line in f:
+                # We support two timestamp formats: "2024-06-01, 09:30" and "Sat Jun 01 09:30:00 UTC 2024"
                 if line[0].isdigit():
                     observed = datetime.datetime.strptime(line[:17], '%Y-%m-%d, %H:%M')
                 else:
                     observed = datetime.datetime.strptime(line[:28], '%a %b %d %H:%M:%S %Z %Y')
                 if i == 0:
+                    # Set a sensible "expected" time for the first observed run.
                     expected = observed.replace(minute=log.hour_offset, second=0)
 
                 # if a cronjob doesn't run within an hour, it gets abandoned.
-                # So this observed may be several expected runs later.
+                # So this "observed" may actually be several expected runs later.
                 while (observed - expected).total_seconds() > max_duration_s:
                     scheduled_runs.append((expected, max_duration_s))
                     expected = next_expected(expected, log.frequency_s)
@@ -85,7 +96,7 @@ def main(debug: bool) -> None:
 
         for i in scheduled_runs:
             if i[1] < 0:
-                print(f"{i[0].strftime('%Y-%m-%d %H:%M')} {int(i[1])} {log.filename}")
+                print(f"{i[0].strftime('%Y-%m-%d %H:%M')} {int(i[1])} <- should have been positive {log.filename}")
 
         with subprocess.Popen(["gnuplot"], stdin=subprocess.PIPE, encoding='utf8') as gnuplot:
             plot_to_png = True
@@ -94,24 +105,27 @@ def main(debug: bool) -> None:
             else:
                 gnuplot.stdin.write("set term block braille size `tput cols`,`tput lines`*4/9\n")
             clean_filename = log.filename.replace('_', '-')
-            gnuplot.stdin.write(f'set label "{clean_filename}" at graph 0.03, 0.9\n')
+            gnuplot.stdin.write(f'set label "{clean_filename}" at graph 0.03, 0.9 front\n')
             gnuplot.stdin.write(f"set xdata time\n")
             gnuplot.stdin.write(f"set timefmt \"%Y-%m-%dT%H-%M\"\n")
-            gnuplot.stdin.write("set xtics rotate by -45\n")
+
+            gnuplot.stdin.write("set xtics out rotate by -45\n")
+            gnuplot.stdin.write("set format x \"%m-%d-%y\"\n")
+            gnuplot.stdin.write('set ylabel "cronjob delay (m)"\n')
             #gnuplot.stdin.write(f"set xrange [{rmin}:{rmax}]\n")
             #gnuplot.stdin.write("set yrange [0:100]\n")
             gnuplot.stdin.write(f"set style fill solid 0.5\n")
+
             gnuplot.stdin.write(f"set key opaque\n")
-            #gnuplot.stdin.write(f"plot '-' using 1:2 title 'cronjob delays in minutes' at 0.19, 0.84 with boxes\n")
-            gnuplot.stdin.write(f"plot '-' using 1:2 title 'cronjob delays in minutes' with boxes\n")
+            gnuplot.stdin.write(f"plot '-' using 1:2 title 'cronjob delays in minutes' with boxes fc 'blue'\n")
             for i in scheduled_runs:
                gnuplot.stdin.write(f"{i[0].strftime('%Y-%m-%dT%H-%M')} {int(i[1]/60)}\n")
             gnuplot.stdin.write("e\n")
             gnuplot.stdin.flush()
+            print(f"Wrote {log.filename}.png")
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Just a template sample.')
     parser.add_argument('-d', '--debug', action='store_true')
-    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
     main(args.debug)
